5d5cae93516585db1dd16e0ee149ff43b151a7b6
Babacar Diop <145564743+BabacarDiop02@users.noreply.github.com>
2025-02-15 13:29:49 +0000

    creation de la page de connection et d'inscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {TemplateComponent} from './pages/template/template.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TemplateComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {InscriptionComponent} from './pages/inscription/inscription.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {ConnectionComponent} from './connection/connection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : 'connection', component : ConnectionComponent},
+  {path : '', component : HomePageComponent},
+  {path : 'home', component : HomePageComponent},
+  {path : 'inscription', component : InscriptionComponent},
+];
